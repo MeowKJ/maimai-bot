933ed054e22809c431aba3bc426000f746188ac3
MeowKJ <ijink@icloud.com>
2023-12-19 15:06:22 +0800

    耗时单位ms->s

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class MyClient(botpy.Client):
                             reply_message = f"{self.robot.name}发现你的b50分数距离上次查询没有变化"
                             await message.reply(file_image=img_path)
                         elif code == 200:
-                            reply_message = f"{self.robot.name}为你生成了新的b50分数图, 耗时{time:.2f}ms"
+                            reply_message = f"{self.robot.name}为你生成了新的b50分数图, 耗时{time:.2f}s"
                             await message.reply(file_image=img_path)
                     else:
                         reply_message = f"{self.robot.name}发现{img}"
